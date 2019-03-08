1a67b7633e0b8c4d5cf4c04616696f9df30fc267
ivujil+2 <malou.alleno22@gmail.com>
2019-03-08 11:20:27 +0100

    version initial du projet

---
+++ build/precache-manifest.7e305470777cc8583201704c140accbb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "a86cab27e3d0e464b250",
+    "url": "./static/js/main.a86cab27.chunk.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "./static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "9435bef6438e593dab0e795cb99a7dfd",
+    "url": "./index.html"
+  }
+];
---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {
+    BrowserRouter,
+    Link,
+    Route,
+    Switch
+} from 'react-router-dom';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h1>Allez Ouste !!</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
