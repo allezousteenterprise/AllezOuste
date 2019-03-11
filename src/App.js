757fc5d55a580b456071116fac2638e15c74d133
ivujil+2 <malou.alleno22@gmail.com>
2019-03-11 10:30:25 +0100

    Ajout de App.js et Index.js

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+    BrowserRouter,
+    Link,
+    Route,
+    Switch
+} from 'react-router-dom';
+
+
+class App extends Component {
+    render() {
+        return (
+		<div>
+                    	<h1>Allez Ouste!!</h1>
+                </div>
+        );
+    };
+}
+
+export default App;
